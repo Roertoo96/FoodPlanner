c3115d16429a970f969491d52086676d7bdb7e98
Felix Breuer <fbreuer@pm.me>
2023-04-01 00:24:20 +0200

    remove csrf
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- FoodPlanner/settings.py
+++ FoodPlanner/settings.py
@@ -20,17 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 debug = os.getenv("DEBUG")
 DEBUG = True if debug else False
 
-ALLOWED_HOSTS = ['127.0.0.1']
-host = os.getenv('HOST')
-if host: ALLOWED_HOSTS = [host]
-
+ALLOWED_HOSTS = ['*']
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 
@@ -44,13 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'FoodGenerator'
-    ]
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
