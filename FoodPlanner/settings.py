6e4eb71930e3d41090bd7587069921b9318ef69a
Felix Breuer <fbreuer@pm.me>
2023-04-01 14:11:57 +0200

    remove csrf
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- FoodPlanner/settings.py
+++ FoodPlanner/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'FoodPlanner.wsgi.application'
-CSRF_TRUSTED_ORIGINS = ['food.tecios.de']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
